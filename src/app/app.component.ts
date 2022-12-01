47c8baa8e29fbc6e2d833194812b11d5a421411b
Бутенко Владислав Александрович <vladislav.butenko@open.ru>
2022-12-01 19:42:38 +0300

    completely done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,29 @@
 import { Component } from '@angular/core';
-import { CardLogo, DataService } from './data.service';
+import { DataService } from './data.service';
+import { catchError, concatMap, EMPTY, from, zip } from "rxjs";
 
 
 @Component({
   selector: 'app-root',
   template: `
-    Logos:
+    Logos: {{ logosString }}
   `,
 })
 export class AppComponent {
 
-  logos: CardLogo[] = [];
+  logosString: string = '';
 
   private ids = [5, 2, 10, 5, 3];
+
+  logosArray$ = zip(from(this.ids).pipe(
+    concatMap((id) => this.dataService.getCard(id).pipe(
+        catchError(() => EMPTY),
+        concatMap((card) => this.dataService.getLogo(card.id).pipe(
+          catchError(() => EMPTY),
+        )),
+      )
+    )
+  )).subscribe((logosArr) => this.logosString = `${logosArr.map((logos) => logos.alt).join(', ')}...` );
 
   constructor(
     private dataService: DataService,
@@ -20,12 +31,16 @@ export class AppComponent {
 
   }
 
+  // 1) from ids
+  // 2) get card
+  // 3) if Y - get logos -> alt, alt
+  // 3) if N - empty if no Card
+  // 4) array -> to format
+  // zip to pack array
+
   /*
   * Get cards by existing ids
   * If card exists get it logo by id and add to logos array
   * Show available card logos alts in template in format "alt, alt, alt..."
   */
-  updateLogos() {
-
-  }
 }
