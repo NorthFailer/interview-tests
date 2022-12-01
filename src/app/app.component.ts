b8cfbdc01c17c4c671f4cea5ed1b64964be01525
NorthFailer <northfailer@gmail.com>
2022-12-01 22:26:32 +0700

    solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,27 @@
-import { Component } from '@angular/core';
-import { Card, DataService } from './data.service';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { DataService } from './data.service';
+import { catchError, of, Subject, switchMap, tap } from 'rxjs';
 
+const ids = [1, 2, 3, 4, 5, 6];
 
 @Component({
   selector: 'app-root',
   template: `
-    <div>The card type: "{{ card?.type }}"</div>
+    <div>The card type: "{{ (card$ | async)?.type }}"</div>
 
     <button (click)="onRandomizeClick()">Randomize</button>
   `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
 
-  cardId?: number;
-  card?: Card;
-
-  readonly ids = [1, 2, 3, 4, 5, 6];
+  readonly randomNumber$ = new Subject<number>();
+  readonly card$ = this.randomNumber$.pipe(
+    switchMap((id) => this.dataService.getData(id).pipe(
+      tap(v => console.log(v)),
+      catchError(() => of(undefined)),
+    )),
+  );
 
   constructor(
     private dataService: DataService,
@@ -24,13 +30,10 @@ export class AppComponent {
   }
 
   onRandomizeClick() {
-    this.cardId = this.ids[Math.floor(Math.random() * this.ids.length)];
+    this.randomNumber$.next(ids[Math.floor(Math.random() * ids.length)]);
   }
 
   /*
   * Method should get card from dataService and update it
   */
-  private updateCard() {
-
-  }
 }
